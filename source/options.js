7beead428c5a108ad651ebc88f29b12e1339385d
jymsy <jym20083617@163.com>
2015-12-14 16:02:48 +0800

    update

--- source/options.js
+++ source/options.js
@@ -12,6 +12,37 @@
 //   options.frequency.disabled = isDeactivated; // The control manipulability.
 // }
 
+function reloadFilter() {
+    var currentFilter = JSON.parse(localStorage.filter);
+    var filterTable = document.querySelector('#filter tbody');
+    filterTable.innerHTML="";
+
+    currentFilter.forEach(function(item) {
+        var filter = document.createElement("tr");
+        filter.innerHTML = "<td>"+item.name+"</td><td>"+item.id+"</td><td><button class='delete'>Delete</button></td>";
+        filterTable.appendChild(filter);
+    });
+}
+
+function removeFilter(filterId) {
+    var deleteIndex;
+    var currentFilter = JSON.parse(localStorage.filter);
+    currentFilter.forEach(function(item) {
+        if (item.id == filterId) {
+
+        }
+    });
+
+    for (var i = currentFilter.length - 1; i >= 0; i--) {
+        if (currentFilter[i].id == filterId) {
+            deleteIndex = i;
+        }
+    }
+
+    currentFilter.splice(i,1);
+    reloadFilter();
+}
+
 window.addEventListener('load', function() {
   // Initialize the option controls.
   // options.isActivated.checked = JSON.parse(localStorage.isActivated);
@@ -21,28 +52,27 @@ window.addEventListener('load', function() {
   freq.value = localStorage.frequency;
                                          // The display frequency, in seconds.
   // if (!options.isActivated.checked) { ghost(true); }
-  // options.filter.value = {"name":"2","value":"dkfjsd"};
-  var currentFilter = JSON.parse(localStorage.filter);
-  var filterTable = document.querySelector('#filter tbody');
 
-  currentFilter.forEach(function(item) {
-    var filter = document.createElement("tr");
-    filter.innerHTML = "<td>"+item.name+"</td><td>"+item.id+"</td><td><button name='delete'>Delete</button></td>";
-    filterTable.appendChild(filter);
-  });
-
+  reloadFilter();
 
   document.querySelector('#add').addEventListener('click', function() {
-    var name = document.getElementById("name").value;
-    var id = document.getElementById("id").value;
+    var name = document.getElementById("name");
+    var id = document.getElementById("id");
 
-    if (name != "" && id != "") {
+    if (name.value != "" && id.value != "") {
         var currentFilter = JSON.parse(localStorage.filter);
-        currentFilter.push({name: name, id:id});
+        currentFilter.push({name: name.value, id:id.value});
         localStorage.filter = JSON.stringify(currentFilter);
+        reloadFilter();
     }
 
+    name.value = "";
+    id.value = "";
   });
+
+    document.querySelector('.delete').addEventListener('click', function() {
+
+    });
   // Set the display activation and frequency.
   // options.isActivated.onchange = function() {
   //   localStorage.isActivated = options.isActivated.checked;
